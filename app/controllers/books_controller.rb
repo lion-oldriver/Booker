305c8e79ec2f8402a7b43b2778394ea287933e6e
lion-oldriver <w879443a72cj2i@gmail.com>
2021-08-06 01:59:31 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
-  def index
-    @books = Book.all
-  end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
   end
 
   def new
@@ -14,18 +15,26 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(book.id)
+    redirect_to book_path(book)
   end
+
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
-  end  
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
 
   private
   def book_params
