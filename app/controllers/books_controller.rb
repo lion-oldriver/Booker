ef0e763fc65b7b968053ac40fbf6a49351937e25
lion-oldriver <w879443a72cj2i@gmail.com>
2021-08-07 06:39:12 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,16 +6,18 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
 
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), flash: {success: 'Book was successfully created.'}
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
 
@@ -24,15 +26,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), flash: {success:'Book was successfully updated.'}
+    else
+      render:edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, flash: {success:'Book was successfully destroyed.'}
   end
 
 
